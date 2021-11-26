7b6f05804aa9ae28b92a4135e51501db377c1f9a
dudwk814 <kj99658103@gmail.com>
2021-11-27 00:05:21 +0900

    Chore adminPage.html 답변 작성 함수 공백 검증 추가
    Chore AnswerDTO.java
    content 필드에 @NotBlank 검증 추가
    
    Chore AnswerController.java createAnswer()
    @valid, BindingResult 추가

--- src/main/java/petppy/controller/answer/AnswerController.java
+++ src/main/java/petppy/controller/answer/AnswerController.java
@@ -4,12 +4,15 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import petppy.dto.answer.AnswerDTO;
 import petppy.service.answer.AnswerService;
+
+import javax.validation.Valid;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,7 +23,11 @@ public class AnswerController {
 
     @PostMapping("/create")
     @PreAuthorize("isAuthenticated() and hasRole('ADMIN')")
-    public ResponseEntity<String> createAnswer(@RequestBody AnswerDTO answerDTO) {
+    public ResponseEntity<String> createAnswer(@RequestBody @Valid AnswerDTO answerDTO, BindingResult result) {
+
+        if (result.hasErrors()) {
+            return new ResponseEntity<>("답변 내용을 입력해주세요.", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
         answerService.createAnswer(answerDTO);
 
         return new ResponseEntity<>("success", HttpStatus.OK);
--- src/main/java/petppy/dto/answer/AnswerDTO.java
+++ src/main/java/petppy/dto/answer/AnswerDTO.java
@@ -5,6 +5,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -12,6 +14,8 @@ import lombok.NoArgsConstructor;
 public class AnswerDTO {
 
     private Long id;
+
+    @NotBlank(message = "답변 내용을 입력해주세요!")
     private String content;
     private Long enquiryId;
 
