01972322582644eaffba5f27eaca4c8f6c0a261e
dudwk814 <kj99658103@gmail.com>
2021-12-09 23:14:02 +0900

    Chore home.html 메인 페이지 디자인 변경
    Chore basicLayout.html
    레이아웃 페이지 하단 디자인 변경
    
    Chore membership.html
    멤버십 페이지 각 멤버십별 서비스 횟수 디자인 변경 및 한글화

--- src/main/java/petppy/PetppyApplication.java
+++ src/main/java/petppy/PetppyApplication.java
@@ -1,6 +1,7 @@
 package petppy;
 
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
