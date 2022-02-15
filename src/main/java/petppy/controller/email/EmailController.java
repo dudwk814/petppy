8799087a6f94ccb954725ed490c6eade72265527
dudwk814 <kj99658103@gmail.com>
2022-02-16 00:30:51 +0900

    Chore EmailServiceImpl.java yml 주입 ignore 설정

--- src/main/java/petppy/controller/email/EmailController.java
+++ src/main/java/petppy/controller/email/EmailController.java
@@ -1,32 +1,32 @@
-//package petppy.controller.email;
-//
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.*;
-//import petppy.dto.email.EmailDTO;
-//import petppy.service.email.EmailService;
-//
-//import static org.springframework.http.HttpStatus.OK;
-//
-//@RestController
-//@RequiredArgsConstructor
-//@RequestMapping("/email/")
-//public class EmailController {
-//
-//    private final EmailService emailService;
-//
-//    @PostMapping("/send")
-//    public ResponseEntity<Long> sendEmail(@RequestBody EmailDTO emailDTO) {
-//
-//
-//        EmailDTO findEmailDTO = emailService.sendEmail(emailDTO);
-//
-//        return new ResponseEntity<>(findEmailDTO.getId(), OK);
-//    }
-//
-//    @PostMapping("/{id}")
-//    public ResponseEntity<Boolean> checkAuthCode(@PathVariable("id") Long id, @RequestBody EmailDTO emailDTO) {
-//        return new ResponseEntity<>(emailService.checkAuthCode(emailDTO), OK);
-//    }
-//}
+package petppy.controller.email;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import petppy.dto.email.EmailDTO;
+import petppy.service.email.EmailService;
+
+import static org.springframework.http.HttpStatus.OK;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/email/")
+public class EmailController {
+
+    private final EmailService emailService;
+
+    @PostMapping("/send")
+    public ResponseEntity<Long> sendEmail(@RequestBody EmailDTO emailDTO) {
+
+
+        EmailDTO findEmailDTO = emailService.sendEmail(emailDTO);
+
+        return new ResponseEntity<>(findEmailDTO.getId(), OK);
+    }
+
+    @PostMapping("/{id}")
+    public ResponseEntity<Boolean> checkAuthCode(@PathVariable("id") Long id, @RequestBody EmailDTO emailDTO) {
+        return new ResponseEntity<>(emailService.checkAuthCode(emailDTO), OK);
+    }
+}
