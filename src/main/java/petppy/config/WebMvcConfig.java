46c4f4e94ec4d13f6f3e7b78e6ef0618f8b57a47
dudwk814 <kj99658103@gmail.com>
2022-02-19 00:19:40 +0900

    Chore WebMvcConfig.java 리눅스 외부 이미지 경로 매핑

--- src/main/java/petppy/config/WebMvcConfig.java
+++ src/main/java/petppy/config/WebMvcConfig.java
@@ -22,7 +22,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler(resourcesUriPath +"/**")
-                .addResourceLocations("file:///" + resourcesLocation);
+                .addResourceLocations("file:/" + resourcesLocation + "/");
 
     }
 
